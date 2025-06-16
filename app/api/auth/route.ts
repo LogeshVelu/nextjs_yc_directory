f42ebfb7476af6241f0379ec639aa64e9eb6dfa8
loegsh <logesh.velu@siemens.com>
2025-06-16 10:42:27 +0530

    handler userd

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,6 +1,8 @@
-import { authOptions } from "@/auth";
-import NextAuth from "next-auth";
+// import { authOptions } from "@/auth";
+// import NextAuth from "next-auth";
 
-export const handler = NextAuth(authOptions);
+// export const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
+import { handlers } from "@/auth" // Referring to the auth.ts we just created
+export const { GET, POST } = handlers;
